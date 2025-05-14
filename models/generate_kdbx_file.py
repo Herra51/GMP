dc0c5db027ab290e2ac869a8232f04cbb24b0ac0
Cantin <cantin.champy@laposte.net>
2025-05-14 16:58:07 +0200

    Enhance KDBX file generation: add error handling for missing credentials, update login to username, and implement password decryption.

--- models/generate_kdbx_file.py
+++ models/generate_kdbx_file.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from flask import request, jsonify
 from pykeepass import create_database, PyKeePass
+from password_generator import PasswordGenerator
 def generate_kdbx(user_id,bdd_connection,master_password):
     try:
         # Récupérer l'ID utilisateur depuis les données POST
@@ -27,18 +28,29 @@ def generate_kdbx(user_id,bdd_connection,master_password):
                     category_name """
         cursor.execute(query, (user_id,))
         user_credentials = cursor.fetchall()
-            
+        if not user_credentials:
+            return jsonify({'error': 'No credentials found for this user'}), 404
+
         cursor.close()
         # Regrouper les données par category_name
         grouped_data = defaultdict(list)
         for credential in user_credentials:
+            print(credential)
             grouped_data[credential['category_name']].append({
                 'id_password': credential['id_password'],
                 'platform_name': credential['platform_name'],
-                'login': credential['login'],
+                'login': credential['login'],  # on remplace login par username
                 'password': credential['password'],
                 'created_at': credential['created_at']
+                
             })
+            # grouped_data[credential['category_name']].append({
+            #     'id_password': credential['id_password'],
+            #     'platform_name': credential['platform_name'],
+            #     'username': credential['login'],  # on remplace login par username
+            #     'password': credential['password'],
+            #     'created_at': credential['created_at']
+            # })
         # Chemin du fichier .kdbx à créer (inclure l'ID utilisateur dans le nom)
         kdbx_file = f'user_{user_id}.kdbx'
 
@@ -47,16 +59,26 @@ def generate_kdbx(user_id,bdd_connection,master_password):
 
         # Charger le fichier KeePass pour y ajouter des entrées
         kp = PyKeePass(kdbx_file, password=master_password)
-
+        
         # Ajouter les groupes et les entrées dans le fichier KeePass
         for category, credentials in grouped_data.items():
+            print(f'Adding group: {category}')
+            print(credentials)
             group = kp.add_group(kp.root_group, category)
             for credential in credentials:
+                print(f'Adding entry: {credential["platform_name"]}')
+                print(credential)
+                try:
+                    decrypted_password = PasswordGenerator.decrypt(credential['password'])
+                except Exception as e:
+                    print(f"Error decrypting password for {credential['platform_name']}: {e}")
+                    decrypted_password = ''  # ou continue pour ignorer cette entrée
+                print(f'Decrypted password: {decrypted_password}')
                 kp.add_entry(
                     group,
                     title=credential['platform_name'],
-                    username=f"user_{credential['id_password']}",
-                    password=credential['password']
+                    username=credential['login'],
+                    password=decrypted_password
                 )
         # Sauvegarder les modifications
         kp.save()
